83574164d49cdb4b543b273d45b056486cb6b9a7
ribinmathew <ribinmathew4u@gmail.com>
2023-03-07 19:08:19 +0530

    admin updated

--- app.js
+++ app.js
@@ -40,30 +40,14 @@ app.use("/", userRouter);
 
 
 
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-// // error handler
 
+app.use((err,req,res,next)=>
+{
 
+console.error(err.stack);
+res.status(500).render("../views/error.ejs")
 
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//  // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-
-// app.use((err,req,res,next)=>
-// {
-
-// console.error(err.stack);
-// res.status(500).render("../views/error.ejs")
-
-// })
+})
 
 
 
